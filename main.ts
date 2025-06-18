9f714fd14cfbbc6661a18b8276d4464a3f9807f6
Lasse1612 <minisaege2006@gmail.com>
2025-06-18 10:18:49 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,3 @@
-let Ziel_x = 0
-let Position_x = 0
-let Position_y = 0
-let Ziel_y = 0
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     Ziel_x += 1
 })
@@ -12,13 +8,21 @@ input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
         	
         } else {
             motors.dualMotorPower(Motor.M0, 50)
-            Position_y += -1
+            Position_y += 1
         }
     } else {
         motors.dualMotorPower(Motor.M0, 50)
-        Position_x += -1
+        Position_x += 1
     }
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     Ziel_y += 1
 })
+let Ziel_y = 0
+let Ziel_x = 0
+let Position_x = 0
+let Position_y = 0
+Position_y = 0
+Position_x = 0
+Ziel_x = 0
+Ziel_y = 0
